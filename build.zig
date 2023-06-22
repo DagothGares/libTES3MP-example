de805d1e23aabfcfde5ca356acc2757950686a4c
Dagoth Gares <63978287+DagothGares@users.noreply.github.com>
2023-06-21 22:53:37 -0500

    Initial commit

--- build.zig
+++ build.zig
@@ -6,8 +6,20 @@ inline fn thisDir() []const u8 {
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+    
+    const libTES3MP = b.addSharedLibrary(.{
+        .name = "libTES3MP-core",
+        .root_source_file = .{ .path = "lib/libTES3MP-core/src/main.zig" },
+        .target = target,
+        .optimize = .ReleaseFast, // libTES3MP currently cannot be run in other release modes.
+    });
+    // Since libTES3MP uses ReleaseFast, there's no need to keep debug info.
+    libTES3MP.strip = true;
+    libTES3MP.override_dest_dir = .{
+        .custom = "deps",
+    };
+    b.installArtifact(libTES3MP);
 
     const lib = b.addSharedLibrary(.{
         .name = "libTES3MP-example",
@@ -15,8 +27,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    
-    lib.addLibraryPath(thisDir() ++ "/libs");
+    lib.addLibraryPath(thisDir() ++ "/zig-out/deps");
+    lib.linkSystemLibrary("libTES3MP-core");
     
     switch(optimize)  {
         .ReleaseFast, .ReleaseSmall => lib.strip = true,
