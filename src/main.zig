0e6d62c78c9d3925148c74d78edbe0bf3e13c41c
Dagoth Gares <63978287+DagothGares@users.noreply.github.com>
2023-06-22 20:29:32 -0500

    update to libTES3MP-core 8550bba

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
-extern "libTES3MP-core" fn impl_logMessage(c_int, ?[*:0]const u8) void;
+extern "libTES3MP-core" fn impl_LogMessage(c_int, ?[*:0]const u8) void;
 
 extern "libTES3MP-core" fn bind_OnServerPostInit(*const fn () callconv(.C) u8) callconv(.C) void;
 
 export fn sayHello() callconv(.C) u8 {
-    impl_logMessage(1, "Hello from the example DLL!");
+    impl_LogMessage(1, "Hello from the example DLL!");
     
     return 3;
 }
